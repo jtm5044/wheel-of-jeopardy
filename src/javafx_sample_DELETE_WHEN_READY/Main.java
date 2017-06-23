29c092a7559d7b9e1bb1417ea39896cd427cc138
Jeremy Martin <jtmartin@mitre.org>
2017-06-23 01:53:00 -0400

    A first cut at the model for the Wheel.

--- src/javafx_sample_DELETE_WHEN_READY/Main.java
+++ src/javafx_sample_DELETE_WHEN_READY/Main.java
@@ -1,10 +1,13 @@
 package javafx_sample_DELETE_WHEN_READY;
 
+import edu.jhu.woj.model.Wheel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -18,6 +21,34 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        ArrayList<String> qb = new ArrayList<String>();
+        qb.add("Hello");
+        qb.add("World");
+        qb.add("Jeopardy");
+        qb.add("Wheel");
+        qb.add("Question");
+        qb.add("Board");
+        Wheel w = new Wheel(qb);
+
+        String[] ws = w.getWheel();
+        ws[1] = "TESTEROO";
+
+        for(int i = 0; i < ws.length; i++)
+        {
+            System.out.println(ws[i]);
+        }
+        System.out.println("-------------------------------------");
+        ws = w.getWheel();
+        for(int i = 0; i < ws.length; i++)
+        {
+            System.out.println(ws[i]);
+        }
+
+        w.setCurrentlySelectedWheelIndex(10);
+        w.setCurrentlySelectedWheelSector("Jeopardy");
+
+        System.out.println(w.toString());
         launch(args);
+
     }
 }
